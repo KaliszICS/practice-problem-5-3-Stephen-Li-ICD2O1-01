27f13d10a0924bbdb1bd11ebf91fc298cf6a5a78
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-05-04 15:00:25 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -11,21 +11,36 @@ public class PracticeProblem {
 
 	}
 
-	public static String[] addString(String[] array1, String word1, int num1) {
+	public static String[] addString(String[] array1, String word1, int index) {
 		String[] array2 = new String[array1.length + 1];
-		for (int i = 0; i < array1.length; i++) {
-			array2[i] = array1[i];
+		int j = 0;
+		for (int i = 0; i < array2.length; i++) {
+			if (i == index) {
+				array2[i] = word1;
+			}
+			else {
+				array2[i] = array1[j];
+				j++;
+			}
 		}
-		array2[num1] = word1;
 		return array2;
 	}
 
 	public static char[] removeChars(char[] array1, char a) {
+		int count = 0;
 		for (int i = 0; i < array1.length; i++) {
-			if (a == array1[i]) {
-				array1[i] = "";
+			if (array1[i] != a) {
+				count++;
 			}
 		}
-		return array1;
+		char [] array2 = new char[count];
+		int j = 0;
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] != a) {
+				array2[j] = array1[i];
+				j++;
+			}
+		}
+		return array2;
 	}
 }
